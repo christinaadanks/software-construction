520d02c9819f598f9a806b69e4103c99cbf27782
christinaadanks <christinaa.danks@me.com>
2021-03-17 23:55:02 -0700

    Add files via upload

---
+++ employee class application/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * @version 3/13/2021
+ * @author Christinaa Danks
+ */
+
+namespace EmployeeApplication
+{
+    class Employee
+    {
+        // Fields
+        public string employeeName, employeeDept, employeePos;
+        public int employeeID;
+
+        // Constructors
+        public Employee(string name, int id, string dept, string pos)
+        {
+            employeeName = name;
+            employeeID = id;
+            employeeDept = dept;
+            employeePos = pos;
+        }
+
+        public Employee(string name, int id)
+        {
+            employeeName = name;
+            employeeID = id;
+            employeeDept = "";
+            employeePos = "";
+        }
+
+        public Employee()
+        {
+            employeeName = "";
+            employeeDept = "";
+            employeePos = "";
+            employeeID = 0;
+        }
+    }
+}
---
+++ employee class application/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace EmployeeApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.employeeButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // employeeButton
+            // 
+            this.employeeButton.BackColor = System.Drawing.Color.LavenderBlush;
+            this.employeeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.employeeButton.Font = new System.Drawing.Font("Apercu Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeeButton.Location = new System.Drawing.Point(261, 59);
+            this.employeeButton.Name = "employeeButton";
+            this.employeeButton.Size = new System.Drawing.Size(278, 51);
+            this.employeeButton.TabIndex = 0;
+            this.employeeButton.Text = "Display Employee Objects";
+            this.employeeButton.UseVisualStyleBackColor = false;
+            this.employeeButton.Click += new System.EventHandler(this.employeeButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightPink;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(180, 162);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(441, 115);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.employeeButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button employeeButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
